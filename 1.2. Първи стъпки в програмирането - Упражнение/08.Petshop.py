5649f9cc337ce5a8234cc7d52e4db918de8791c2
TinaZhelyazova <radithepianist@gmail.com>
2023-01-11 21:53:32 +0200

    dunno

--- 1.2. Първи стъпки в програмирането - Упражнение/08.Petshop.py
+++
@@ -1,8 +0,0 @@
-dog_food_needed = int(input())
-cat_food_needed = int(input())
-
-dog_food = 2.50
-cat_food = 4.00
-
-expenses = (dog_food*dog_food_needed) + (cat_food*cat_food_needed)
-print(f"{expenses} lv.")
