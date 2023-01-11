f10a78f2062322d8c7eccf81d278ff28e3c39eef
TinaZhelyazova <radithepianist@gmail.com>
2023-01-11 21:56:10 +0200

    First Steps in Coding - Exercises

---
+++ 1.1. Първи стъпки в програмирането - Упражнение/07. Projects Creation.py
@@ -0,0 +1,5 @@
+name = input()
+projects_number = int(input())
+hours_needed = 3*projects_number
+
+print(f"The architect {name} will need {hours_needed} hours to complete {projects_number} project/s.")
---
+++ 1.1. Първи стъпки в програмирането - Упражнение/09. Yard Greening.py
@@ -0,0 +1,5 @@
+square_meters = float(input())
+final_sum = square_meters*7.61
+discount = final_sum * (18/100)
+print(f"The final price is: {final_sum-discount}")
+print(f"The discount is: {discount}")
